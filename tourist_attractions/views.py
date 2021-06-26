a44665f4dc8894453f04dc4e9e1e08aae2eacbbe
Tiberiu <danielt.neagoe@gmail.com>
2021-06-26 11:39:36 -0400

    End of class 6

--- tourist_attractions/views.py
+++ tourist_attractions/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
+attractions_list = [
+    {'attraction_name': "Niagara Falls", "state": "New York"},
+    {'attraction_name': "Grand Canyon National Park", "state": "Arizona"},
+    {'attraction_name': "Mall of America", "state": "Minnesota"},
+    {'attraction_name': "Mount Rushmore", "state": "South Dakota"},
+    {'attraction_name': "Times Square", "state": "New York"},
+    {'attraction_name': "Walt Disney World", "state": "Florida"},
+    {'attraction_name': "Test Attraction", "state": "Quebec"},
+]
+
 
 def home(request):
-    return render(request, 'tourist_attractions/home.html')
+    context = {"attractions": attractions_list}
+    return render(request, 'tourist_attractions/home.html', context)
+
+
+def details(request, statename):
+    context = {"attractions": attractions_list, "statename": statename.replace("-", " ")}
+    return render(request, 'tourist_attractions/details.html', context)
+
